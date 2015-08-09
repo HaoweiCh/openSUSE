68b2690f87bf178cf6de5b96f0bcdce95a665c5d
HeawerCher <HeawerCher@gmail.com>
2015-08-09 19:18:54 +0800

    vesion clean

--- auto.py
+++ auto.py
@@ -1,72 +1,125 @@
 #! /usr/bin/python3.4
 #
-import os
+import os,sys
 import subprocess
-savepath = '/home/Che/Projects/openSUSE/.backup/'
-paths = [
-'~/.themes/',
-'~/.fonts/',
-'~/.icons/',
-'~/.conky/',
-'~/.conkyrc',
-'~/vimwiki/',
-#############
-#'/usr/share/icons'
-]
 
+cmd = 'cd  && pwd';(status, output) = subprocess.getstatusoutput(cmd);pwdu = output
+if (not pwdu.endswith('/')):pwdu = pwdu +'/'
+pwd = os.path.abspath('')
+if (not pwd.endswith('/')):pwd = pwd +'/'
+del cmd,status,output
+
+savepath = '/home/Che/Projects/openSUSE/.backup/'
+def savepath_():
+    global savepath
+    savepath = pwd + '.backup/'
+savepath_();#print(savepath)
+
+paths = [
+#(originpath,savedpath)
+('~/.themes/',      savepath),
+('~/.fonts/',       savepath),
+('~/.icons/',       savepath),
+('~/.conky/',       savepath),
+('~/.conkyrc',      savepath),
+('~/vimwiki/',      savepath),
+#############
+('/usr/share/icons/',    savepath + 'usr/share/'),
+('/usr/share/themes/',  savepath + 'usr/share/')
+]
 def paths_():
     global paths
-    cmd = 'cd  && pwd'
-    (status, output) = subprocess.getstatusoutput(cmd)
-    pwd = output
-    if (not pwd.endswith('/')):pwd = pwd +'/'
-    #print (pwd)
+    global pwdu
+    #print (pwdu)
+
     temp=[]
-    for path in paths:
-        if (path.startswith('~/')):
-            path = pwd + path[2:]
-        temp.append (path)
-    paths = temp
+    for (opath,spath) in paths:
+        if (opath.startswith('~/')):
+            opath = pwdu + opath[2:]
+        temp.append ((opath,spath))
+    paths = temp;#print (paths,'\n',temp)
 paths_()
+###
 
+
+###
 def backup():
-
-
-    temp=[]
-    for path in paths:
-        if (not os.path.exists(path)):next
-        isfile = os.path.isfile(path)
-        isdir = os.path.isdir(path)
+    for (opath,spath) in paths:
+        if (not os.path.exists(opath)):next
+        if (not os.path.exists(spath)):cmd = 'mkdir -p '+spath;subprocess.getstatusoutput(cmd)
+        isfile = os.path.isfile(opath)
+        if (isfile):filename = opath.split('/')[-1]
+        isdir = os.path.isdir(opath)
+        if(isdir):dirname = os.path.dirname(opath).split('/')[-1]
 
         if (isfile):
-            filename = path.split('/')[-1]
             #print ('check file {0}|{1}'.format(filename,path))
             #subprocess.Popen('cp -u {0} {1}'.format(path,savepath+filename),shell=True).wait()
-            cmd = 'cp -u {0} {1}'.format(path,savepath)
+            cmd = 'cp -u {0} {1}'.format(opath,spath)
             (status, output) = subprocess.getstatusoutput(cmd)
-            if (not status):
-                print('succeeded\t copy file:\t{0}'.format(filename))
-            else:
-                print('failed   \t copy file:\t{0}'.format(filename))
+            print ('status: %-3s dir: %-6s'%(status,filename))
+            if (status):print (' '*3,output)
+            #if (not status):
+            #    print('succeeded\t copy file:\t{0}'.format(filename))
+            #else:
+            #    print('failed   \t copy file:\t{0}'.format(filename))
+            #
         elif(isdir):
-            dirname = os.path.dirname(path).split('/')[-1]
+
             #subprocess.Popen('cp -urv {0} {1}'.format(path,savepath+dirname),shell=True).wait()
-            cmd ='cp -urv {0} {1}'.format(path,savepath)
+            cmd ='cp -urv {0} {1}'.format(opath,spath)
             (status, output) = subprocess.getstatusoutput(cmd)
-            if (not status):
-                print('succeeded\t copy dir:\t{0}'.format(dirname))
-            else:
-                print('failed    \t copy  dir:\t{0}'.format(dirnme))
+            print ('status: %-3s dir: %-8s'%(status,dirname))
+            if (status):print (' '*3,output)
+            #if (not status):
+            #    print('succeeded\t copy dir:\t{0}'.format(dirname))
+            #else:
+            #    print('failed    \t copy  dir:\t{0}'.format(dirnme))
         else:
-            print ('path:{0} \tignored'.format(path))
+            #print ('path:{0} \tignored'.format(opath))
+            print ( 'ignored \t %s' % (opath) )
 def recovery():
-    pass
+    for (opath,spath) in paths:
+        if (opath.endswith('/')):
+            dirname = os.path.abspath(opath)
+            dirname =os.path.abspath(dirname)
+        else:
+            dirname = os.path.abspath(opath)
+        name = dirname.split('/')[-1]
+        #print(name,dirname)
+
+        if (not os.path.exists(spath + name)):
+            print ('ignored \t%s'%(dirname))
+            next
+
+        cmd = 'sudo cp -ru {0} {1}'.format(spath + name,os.path.dirname(dirname));#print(cmd)
+        status = subprocess.Popen(cmd,shell = True).wait()
+        print ('status: %-3s dir: %-6s'%(status,dirname))
+
+
+
+
+###
+def test():
+    for (opath,spath) in paths:
+        cmd = 'sudo rm -rf %s'%(opath)
+        if(not subprocess.Popen(cmd,shell=True).wait()):
+            print ('Succeeded')
+        else:
+            print ('Failed')
+    return ''
+
+def defi(a = '', b =[]):
+    for arg in b:
+        if(a==arg):
+            return True
 if __name__ == '__main__':
-    import sys
-    if ( sys.argv[-1]=='-b'):
+    if (defi(sys.argv[-1],['-b','b','--back'])):
         backup()
-    elif (sys.argv[-1]=='-r'):
-        recovery()
+    elif ( defi(sys.argv[-1],['-r','r','--recovery']) ):
+        recovery();#print('called reconvery')
+    elif(sys.argv[-1] == 'test'):
+        test()
     else:
         print ('''--------------------------------
 Auto backup the important data_
